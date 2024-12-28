77baecb4acda0c75954d025a9df0ee6706bee697
Victoria Shevchenko <shevviktoryia@gmail.com>
2024-12-28 11:01:25 +0300

    feat: add project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { CounterField } from "./components/CounterField";
+import { ControlButtons } from "./components/ControlButtons";
+import styled from "styled-components";
+import { ProgressBar } from "./components/ProgressBar";
 
 function App() {
+  const maxCount = 5;
+  const minCount = 0;
+  let [count, setCount] = useState<number>(minCount);
+
+  const increaseCountHandler = () =>
+    count < 5
+      ? setCount((prevCount) => Math.min(prevCount + 1, maxCount))
+      : count;
+  const resetCountHandler = () => {
+    setCount(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CounterStyled>
+        <CounterField count={count} />
+        <ProgressBar count={count} maxCount={maxCount} />
+        <ControlButtons
+          minCount={minCount}
+          maxCount={maxCount}
+          count={count}
+          updateCount={increaseCountHandler}
+          resetCount={resetCountHandler}
+        />
+      </CounterStyled>
     </div>
   );
 }
+
+const CounterStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  border: 2px solid #1babdb;
+  padding: 10px;
+  border-radius: 10px;
+`;
 
 export default App;
---
+++ src/components/ControlButtons.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { Button } from "./Button";
+
+type ButtonProps = {
+  count: number;
+  maxCount: number;
+  minCount: number;
+  updateCount?: () => void;
+  resetCount?: () => void;
+};
+
+export const ControlButtons = (props: ButtonProps) => {
+  return (
+    <ControlButtonsStyled>
+      <Button
+        title="increase"
+        updateCount={props.updateCount}
+        isDisable={props.count === props.maxCount}
+      />
+      <Button
+        title="reset"
+        isDisable={props.count === props.minCount}
+        resetCount={props.resetCount}
+      />
+    </ControlButtonsStyled>
+  );
+};
+
+const ControlButtonsStyled = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-top: 20px;
+  justify-content: center;
+`;
